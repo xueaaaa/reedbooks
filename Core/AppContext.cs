3f23d15dd3e9cb2fe200fe1dad1a50e9e0f2a902
xueaaaa <ss.bazunov@gmail.com>
2023-10-12 21:51:28 +0300

    add: database application context

---
+++ Core/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReedBooks.Models.Book;
+
+namespace ReedBooks.Core
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=reedbooks_data.db");
+        }
+    }
+}
