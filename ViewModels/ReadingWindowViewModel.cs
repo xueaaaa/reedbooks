014a23eb1a79c7a2585a1eb3ac8006a93ed632c5
xueaaaa <ss.bazunov@gmail.com>
2023-10-14 13:57:59 +0300

    add: reading window

---
+++ ViewModels/ReadingWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReedBooks.Core;
+using ReedBooks.Models.Book;
+
+namespace ReedBooks.ViewModels
+{
+    public class ReadingWindowViewModel : ObservableObject
+    {
+        public Book ReadingBook { get; set; }
+
+        public ReadingWindowViewModel()
+        {
+        }
+
+        public ReadingWindowViewModel(Book readingBook) 
+        {
+            ReadingBook = readingBook;
+        }
+    }
+}
---
+++ Views/ReadingWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ReedBooks.Models.Book;
+using ReedBooks.ViewModels;
+using System.Windows;
+
+namespace ReedBooks.Views
+{
+    public partial class ReadingWindow : Window
+    {
+        public ReadingWindow(Book readingBook)
+        {
+            InitializeComponent();
+
+            DataContext = new ReadingWindowViewModel(readingBook);
+        }
+    }
+}
