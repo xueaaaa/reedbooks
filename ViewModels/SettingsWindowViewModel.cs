22857c05b6099db0407a5c5a83e4ff60230b60ff
xueaaaa <ss.bazunov@gmail.com>
2023-10-25 20:21:46 +0300

    add: settings window

---
+++ ViewModels/SettingsWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using ReedBooks.Core;
+using System.Collections.ObjectModel;
+using System.Globalization;
+
+namespace ReedBooks.ViewModels
+{
+    public class SettingsWindowViewModel : ObservableObject
+    {
+        private ObservableCollection<CultureInfo> _appLanguages;
+        public ObservableCollection<CultureInfo> AppLanguages
+        {
+            get => _appLanguages;
+        }
+
+        private CultureInfo _selectedLanguage;
+        public object SelectedLanguage
+        {
+            get => _selectedLanguage;
+            set
+            {
+                if (value != null)
+                {
+                    _selectedLanguage = (CultureInfo)value;
+                    OnPropertyChanged(nameof(SelectedLanguage));
+                }
+            }
+        }
+
+        public SettingsWindowViewModel()
+        {
+            _appLanguages = new ObservableCollection<CultureInfo>(App.AppLanguages);
+            _selectedLanguage = App.CurrentLanguage;
+        }
+    }
+}
---
+++ Views/SettingsWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ReedBooks.ViewModels;
+using System.Windows;
+
+namespace ReedBooks.Views
+{
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            DataContext = new SettingsWindowViewModel();
+        }
+    }
+}
