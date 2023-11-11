3ece71787541e033fa6662d5dcc019aa5f2cdbfa
hnnngn <ss.bazunov@gmail.com>
2023-11-11 21:58:50 +0300

    add: some changes

---
+++ Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System;
+
+namespace ReedBooks.Converters
+{
+    public class EnumToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
