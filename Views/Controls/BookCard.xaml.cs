0e82208e7c7e1ae05468393da3450c14da4cbb5a
xueaaaa <ss.bazunov@gmail.com>
2023-11-13 21:34:17 +0300

    add: book card element

---
+++ Views/Controls/BookCard.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ReedBooks.Models.Book;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ReedBooks.Views.Controls
+{
+    public partial class BookCard : Border
+    {
+        public static readonly DependencyProperty BookProperty =
+            DependencyProperty.Register("Book", typeof(Book), typeof(BookCard));
+
+        public Book Book
+        {
+            get => (Book)GetValue(BookProperty);
+            set => SetValue(BookProperty, value);
+        }
+
+        public BookCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
