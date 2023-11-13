acc3f3464eb39defdd0f65219f2328c52025814f
xueaaaa <ss.bazunov@gmail.com>
2023-11-13 22:34:40 +0300

    add: settting parameter control

---
+++ Views/Controls/SettingsParameter.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ReedBooks.Views.Controls
+{
+    public partial class SettingsParameter : Grid
+    {
+        public static readonly DependencyProperty ParameterNameProperty =
+            DependencyProperty.Register("ParameterName", typeof(string), typeof(SettingsParameter));
+        public static readonly DependencyProperty ParameterHintProperty =
+            DependencyProperty.Register("ParameterHint", typeof (string), typeof(SettingsParameter));
+        public static readonly DependencyProperty ValueSelectorProperty =
+            DependencyProperty.Register("ValueSelector", typeof(FrameworkElement), typeof(SettingsParameter));
+
+        public string ParameterName
+        {
+            get => (string)GetValue(ParameterNameProperty);
+            set => SetValue(ParameterNameProperty, value);
+        }
+
+        public string ParameterHint
+        {
+            get => (string)GetValue(ParameterHintProperty);
+            set => SetValue(ParameterHintProperty, value);
+        }
+
+        public FrameworkElement ValueSelector
+        {
+            get => (FrameworkElement)GetValue(ValueSelectorProperty);
+            set => SetValue(ValueSelectorProperty, value);
+        }
+
+        public SettingsParameter()
+        {
+            InitializeComponent();
+        }
+    }
+}
