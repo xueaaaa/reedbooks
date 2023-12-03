5d23d2e1c5e43a63e9eff6cbec1b521b486a99c1
xueaaaa <ss.bazunov@gmail.com>
2023-12-03 18:11:20 +0300

    add: parsing version from github

--- Core/Version/Version.cs
+++ Core/Version/Version.cs
@@ -1,7 +1,30 @@
-﻿namespace ReedBooks.Core.Version
+﻿using Octokit;
+using System;
+using System.Reflection;
+
+namespace ReedBooks.Core.Version
 {
     public struct Version
     {
+        private const string GITHUB_REPOSITORY_NAME = "ReedBooks";
+        private const string GITHUB_REPOSITORY_OWNER = "xueaaaa";
+
+        /// <summary>
+        /// Program version recorded in Assembly
+        /// </summary>
+        public static Version Local
+        {
+            get
+            {
+                string version = Assembly.GetExecutingAssembly()
+                    .GetName()
+                    .Version
+                    .ToString();
+
+                return new Version(version);
+            }
+        }
+
         /// <summary>
         /// First digit in the sequence (x.0.0.0)
         /// </summary>
@@ -19,36 +42,38 @@
         /// </summary>
         public byte Revision;
 
-        public Version(byte major)
-        {
-            Major = major;
-            Minor = 0;
-            Patch = 0;
-            Revision = 0;
-        }
-
-        public Version(byte major, byte minor)
-        {
-            Major = major;
-            Minor = minor;
-            Patch = 0;
-            Revision = 0;
-        }
-
-        public Version(byte major, byte minor, byte patch)
-        {
-            Major = major;
-            Minor = minor;
-            Patch = patch;
-            Revision = 0;
-        }
-
-        public Version(byte major, byte minor, byte patch, byte revision)
+        public Version(byte major, byte minor = 0, byte patch = 0, byte revision = 0)
         {
             Major = major;
             Minor = minor;
             Patch = patch;
             Revision = revision;
+        }
+
+        public Version(string stringVersion)
+        {
+            var versionParts = stringVersion.Split('.');
+            if (versionParts.Length != 4) this = new Version(0);
+
+            Major = Convert.ToByte(versionParts[0]);
+            Minor = Convert.ToByte(versionParts[1]);
+            Patch = Convert.ToByte(versionParts[2]);
+            Revision = Convert.ToByte(versionParts[3]);
+        }
+
+        /// <summary>
+        /// Gets the latest version of the program from GitHub
+        /// </summary>
+        /// <returns>Version</returns>
+        public static Version GetFromGitHub()
+        {
+            var github = new GitHubClient(new ProductHeaderValue(GITHUB_REPOSITORY_NAME));
+            var tags = github.Repository.GetAllTags(GITHUB_REPOSITORY_OWNER, GITHUB_REPOSITORY_NAME).Result;
+            var latestTag = tags[tags.Count - 1];
+            var versionAsStr = latestTag.Name;
+            var version = new Version(versionAsStr);
+
+            return version;
         }
 
         public static bool operator >(Version a, Version b)
