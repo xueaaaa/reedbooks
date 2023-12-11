bea9fb963679b77a1e0b37a467184145e1b56c1e
xueaaaa <ss.bazunov@gmail.com>
2023-12-11 22:17:14 +0300

    add: e.assessment selector design

---
+++ Views/Controls/EmotionalAssessmentControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ReedBooks.Views.Controls
+{
+    public partial class EmotionalAssessmentControl : StackPanel
+    {
+        public static readonly DependencyProperty SetEmoteCommandProperty =
+            DependencyProperty.Register("SetEmoteCommand", typeof(ICommand), typeof(EmotionalAssessmentControl));
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(EmotionalAssessmentControl));
+
+        public ICommand SetEmoteCommand
+        {
+            get => (ICommand)GetValue(SetEmoteCommandProperty);
+            set => SetValue(SetEmoteCommandProperty, value);
+        }
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public EmotionalAssessmentControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
