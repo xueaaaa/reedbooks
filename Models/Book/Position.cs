4023dca006a8d2b6906ad5d5fe1b6f30f5dfe654
xueaaaa <ss.bazunov@gmail.com>
2023-12-17 20:24:59 +0300

    small fixes

---
+++ Models/Book/Position.cs
@@ -0,0 +1,44 @@
+﻿using ReedBooks.Models.Database;
+using ReedBooks.Views.Controls;
+using System;
+
+namespace ReedBooks.Models.Book
+{
+    public class Position : DependentDatabaseObject
+    {
+        private NavigationItem _chapter;
+        public NavigationItem Chapter
+        {
+            get => _chapter;
+            set
+            {
+                _chapter = value;
+                OnPropertyChanged(nameof(Chapter));
+            }
+        }
+
+        private double _verticalOffset;
+        public double VerticalOffset
+        {
+            get => _verticalOffset;
+            set
+            {
+                _verticalOffset = value;
+                OnPropertyChanged(nameof(VerticalOffset));
+            }
+        }
+
+        public Position()
+        {
+            
+        }
+
+        public Position(Guid targetGuid, NavigationItem chapter, double verticalOffset) : this()
+        {
+            Guid = new Guid();
+            TargetGuid = targetGuid;
+            Chapter = chapter;
+            VerticalOffset = verticalOffset;
+        }
+    }
+}
