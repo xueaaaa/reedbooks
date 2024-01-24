585d927185f26fcd66ff59217cac330a43a7ab96
xueaaaa <ss.bazunov@gmail.com>
2024-01-24 23:03:23 +0300

    upd: design

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.1")]
-[assembly: AssemblyFileVersion("0.2.0.1")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
 [assembly: NeutralResourcesLanguage("ru")]
